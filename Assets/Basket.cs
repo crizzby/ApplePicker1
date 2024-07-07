2fef51e0f7a55e38475c29350599cd195d6a1bd3
crizzby <joel.wekker@gmx.de>
2024-07-07 22:36:03 +0200

    Added Scripts
    Added Movement to my Basket to catch the Apples

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //work in progress    
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;                                   //a
+
+        //The Camera's z postion sets how far to push the mouse into 3D
+        //If this line causes a NullReferenceException, select the Main Camera
+        //in the Hierachy and set its tag to MainCamera in the Inspector.
+        mousePos2D.z = -Camera.main.transform.position.z;                            //b
+
+        //Convert the point from 2D screen space innto 3D game world space 
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);            //c
+
+        //Move hthe x position of this Basket to the x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
         
     }
+
+    void OnCollisionEnter(Collision coll)
+        {
+            //Find out what hit this basket
+            GameObject collideWith = coll.gameObject;
+            if(collideWith.CompareTag("Apple"))
+            {
+                Destroy(collideWith);
+            }
+        }
 }
