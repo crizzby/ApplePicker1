4f19b7b087a3f47b1cb9b08cffca84945e3c5207
crizzby <joel.wekker@gmx.de>
2024-07-08 18:34:13 +0200

    Added loosing "life"

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,17 +10,46 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for(int i=0; i <numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos =Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }    
+    }
+
+    public void AppleMissed()
+    {
+        //Destroy all of the falling apples
+        GameObject[] appleArray=GameObject.FindGameObjectsWithTag("Apple"); //b
+        foreach (GameObject  tempGo in appleArray)
+        {
+            Destroy(tempGo);
+        }
+        //Destroy one of the Baskets
+        //Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count -1;
+        
+        //Get a reference to that Basket GameObject
+        GameObject basketGO = basketList[basketIndex];
+        
+        //remove the Basket from the List and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(basketGO);
+
+        //if there is no Baskets left the game restarts
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("_Scene_0");
+        }
     }
 
     // Update is called once per frame
